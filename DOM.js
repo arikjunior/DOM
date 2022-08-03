1e29fe7fc8753003f7ba756aa77f1246b59bc211
arikjunior <arik.junior11@gmail.com>
2022-08-03 14:38:11 +0700

    learn DOM

---
+++ DOM.js
@@ -0,0 +1,31 @@
+//DOM (Document Object Model) adalah Representasi element HTML didalam document menjadi sebuah object
+
+//DOM adalah Antarmuka pemrograman berbasis object yang mempresentasikan dokumen web
+
+//DOM membuat seluruh komponen dari halaman web dapat diakses dan dimanipulasi
+
+//Komponen :
+// element HTML
+// atribut
+// text
+// dll
+
+//DOM dapat dimanipulasi (dibuat, diubah, dihapus) menggunakan javascript
+
+
+//Struktur Hierarki DOM
+
+//- Root Node :
+//  node yang menjadi sumber dari semua node lain didalam DOM
+// document
+
+//- Parent Node :
+//  node yang berada 1 tingkat diatas node yang lain
+// body adalah parent dari h1,p,p,div dan a
+
+//- Child Node :
+//  node yang berada 1 tingkat dibawah node yang lain
+//  h1 adalah child dari body
+
+
+console.log(document);
