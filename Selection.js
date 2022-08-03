4c148a4a6a278012f6ec5764ffc0cc973b08656d
arikjunior <arik.junior11@gmail.com>
2022-08-03 15:41:19 +0700

    selection

---
+++ Selection.js
@@ -0,0 +1,38 @@
+//DOM Selection Method
+
+// getElementById() // Memilih element berdasarkan atribut id
+// getElementsByTagName() //Mencari element-element dengant tag name tertentu
+// getElementsByClassName() //Mencari element-element dengant nama class tertentu
+// querySelector() // memilih satu element yang dipilih atau memilih selector CSS pada dokumen HTML
+// querySelectorALL() // memilih semua yang dipilih atau memilih selector CSS pada dokumen HTML
+
+
+// getElementById() // element
+// getElementsByTagName() // HTML Collection
+// getElementsByClassName() // HTML Collection
+// querySelector() // element
+// querySelectorALL() // nodeList
+
+
+
+const judul = document.getElementById('judul');
+
+judul.style.color = 'red';
+judul.style.backgroundColor = 'grey';
+judul.innerHTML = 'Arik Junior';
+
+
+const p = document.getElementsByTagName('p');
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'lightblue';
+
+}
+const h1 = document.getElementsByTagName('h1')[0];
+
+h1.style.fontSize = '50px';
+
+
+const p1 = document.getElementsByClassName('p1')[0];
+
+p1.innerHTML = 'ini diubah dari javascript';
