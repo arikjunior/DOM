142f67ccd4d72fa2240e4ffd49932af1e0d6f6c0
arikjunior <arik.junior11@gmail.com>
2022-08-09 15:11:14 +0700

    manipulation

---
+++ Manipulation.js
@@ -0,0 +1,31 @@
+////element.innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Arik Junior</em>';
+
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world!'
+
+
+////element.style.<property>
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue'
+// judul.style.backgroundColor = 'salmon'
+
+
+////element.setAttribute
+// const judul = document.getElementsByTagName('h1')[0];
+// const a = document.querySelector('section#a a');
+// a.setAttribute('id', 'link') // menambah id = "link"
+
+
+
+////element.classList.
+
+const p2 = document.querySelector('.p2');
+p2.elemenet.classList.add()
+p2.elemenet.classList.remove()
+p2.elemenet.classList.toggle()
+p2.elemenet.classList.item()
+p2.elemenet.classList.contains()
+p2.elemenet.classList.replace()
