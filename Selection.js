b0e599372425e2c1ab4421bbb5937773a59ce5f8
arikjunior <arik.junior11@gmail.com>
2022-08-09 14:35:19 +0700

    selection

--- Selection.js
+++ Selection.js
@@ -14,25 +14,47 @@
 // querySelectorALL() // nodeList
 
 
+//////////////////
+// const judul = document.getElementById('judul');
 
-const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = 'grey';
+// judul.innerHTML = 'Arik Junior';
 
-judul.style.color = 'red';
-judul.style.backgroundColor = 'grey';
-judul.innerHTML = 'Arik Junior';
+// ///////////////////
+// const p = document.getElementsByTagName('p');
+
+// for (let i = 0; i < p.length; i++) {
+//     p[i].style.backgroundColor = 'lightblue';
+
+// }
+// const h1 = document.getElementsByTagName('h1')[0];
+
+// h1.style.fontSize = '50px';
+
+// //////////////////////
+// const p1 = document.getElementsByClassName('p1')[0];
+
+// p1.innerHTML = 'ini diubah dari javascript';
 
 
-const p = document.getElementsByTagName('p');
+/////////////////////
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green'
+// p4.style.fontSize = '30px'
 
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+
+// li2.style.backgroundColor = 'orange'
+
+// const p = document.querySelector('p');
+// p.innerHTML = 'Ini diubah menggunakan javascript'
+
+
+
+
+////////////////////
+const p = document.querySelectorAll('p');
 for (let i = 0; i < p.length; i++) {
-    p[i].style.backgroundColor = 'lightblue';
-
+    p[i].style.backgroundColor = 'lightblue'
 }
-const h1 = document.getElementsByTagName('h1')[0];
-
-h1.style.fontSize = '50px';
-
-
-const p1 = document.getElementsByClassName('p1')[0];
-
-p1.innerHTML = 'ini diubah dari javascript';
